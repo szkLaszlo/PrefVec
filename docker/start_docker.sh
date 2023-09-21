260d745526c4ece7359bff0c624dc5191770d6b1
Szoke <szoke.laszlo95@gmail.com>
2023-09-21 17:44:38 +0200

    move workspace warning

--- docker/start_docker.sh
+++ docker/start_docker.sh
@@ -2,20 +2,20 @@
 export UID
 export USER
 
-if [ -z "$WORKSPACE" ]; then
-    echo "The WORKSPACE environment variable is not set."
-    read -p "Please enter the value for WORKSPACE: " WORKSPACE
-    export WORKSPACE
-    echo "WORKSPACE set to: $WORKSPACE"
-else
-    echo "WORKSPACE is already set to: $WORKSPACE"
-fi
-
 docker_list_raw=`docker ps --filter "name=docker_sumo" -q`
 docker_list=($docker_list_raw)
 
 if [ ${#docker_list[@]} -eq 0 ]; then
         echo "We have no running docker container, so we start one."
+	if [ -z "$WORKSPACE" ]; then
+	   echo "The WORKSPACE environment variable is not set."
+	   read -p "Please enter the value for WORKSPACE: " WORKSPACE
+	   export WORKSPACE
+	   echo "WORKSPACE set to: $WORKSPACE"
+	else
+	   echo "WORKSPACE is already set to: $WORKSPACE"
+	fi
+
         docker compose -f ./docker-compose.yml run --name docker_sumo --rm --service-ports sumo
 else
         echo "We are entering the only running docker container."
