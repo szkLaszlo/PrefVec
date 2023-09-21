da0af0ed3d46537cd9e2062fcfc77793ef3ee7c0
Szoke Laszlo <szoke.laszlo95@gmail.com>
2023-09-21 17:40:20 +0200

    chg: workspace variable is added

--- docker/start_docker.sh
+++ docker/start_docker.sh
@@ -2,6 +2,15 @@
 export UID
 export USER
 
+if [ -z "$WORKSPACE" ]; then
+    echo "The WORKSPACE environment variable is not set."
+    read -p "Please enter the value for WORKSPACE: " WORKSPACE
+    export WORKSPACE
+    echo "WORKSPACE set to: $WORKSPACE"
+else
+    echo "WORKSPACE is already set to: $WORKSPACE"
+fi
+
 docker_list_raw=`docker ps --filter "name=docker_sumo" -q`
 docker_list=($docker_list_raw)
 
@@ -10,5 +19,5 @@ if [ ${#docker_list[@]} -eq 0 ]; then
         docker compose -f ./docker-compose.yml run --name docker_sumo --rm --service-ports sumo
 else
         echo "We are entering the only running docker container."
-	docker exec -it -e HOST_USER=$USER -e HOST_UID=$UID -u 0 ${docker_list[0]} /user_entry.sh
+	docker exec -it -e HOST_USER=$USER -e HOST_UID=$UID -e WORKSPACE=$WORKSPACE -u 0 ${docker_list[0]} /user_entry.sh
 fi
