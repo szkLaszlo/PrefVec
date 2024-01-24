a51995ffd2794c064eedc1b355bb999820e2715a
Szoke Laszlo <szoke.laszlo95@gmail.com>
2024-01-24 18:26:32 +0100

    chg: tb plotter now supports different type of keys to log

--- utils/tensorboard_plots.py
+++ utils/tensorboard_plots.py
@@ -3,8 +3,8 @@ import pickle
 
 import matplotlib.pyplot as plt
 import numpy as np
-from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 import pandas as pd
+from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 
 
 # Function to load tensorboard data
@@ -21,7 +21,7 @@ def load_tensorboard_data(logdir):
             events = event_acc.Scalars(tag)
             steps, values = zip(*[(event.step, event.value) for event in events])
             data[tag] = pd.DataFrame({tag: values}, index=steps)
-        event_data.append(pd.concat(data.values(), axis=1, join='inner'))
+        event_data.append(pd.concat(data.values(), axis=1))
 
     return pd.concat(event_data, axis=1)
 
@@ -44,61 +44,76 @@ if __name__ == "__main__":
     # Folder containing tensorboard files
     folder_path = '/cache/PrefVeC_results_forv2/extra_algos/'
     saved_file_name = "data_extract_pd"
-    timestep_key = "ray/tune/episodes_total"
+    timestep_keys = ["ray/tune/episodes_total", ]
     # List all subdirectories (assuming each subdirectory corresponds to a different model)
     subdirectories = [os.path.join(folder_path, name) for name in os.listdir(folder_path) if
                       os.path.isdir(os.path.join(folder_path, name))]
     extract_data_from_event_files(subdirectories, save_file_name=saved_file_name, overwrite=False)
-    names_for_y_axes = {"ray/tune/evaluation/episode_reward_mean": "Mean Episode Reward",
-                        "ray/tune/evaluation/episode_len_mean": "Mean Episode Length",
-                        "ray/tune/evaluation/custom_metrics/success_mean": "Success Rate",
-                        "ray/tune/evaluation/custom_metrics/slow_mean": "Slow Rate",
-                        "ray/tune/evaluation/custom_metrics/collision_mean": "Collision Rate",
+
+    # reverse key and value for names_for_y_axes
+    names_for_y_axes = {"Mean Episode Reward": ["ray/tune/evaluation/episode_reward_mean"],
+                        "Mean Episode Length": ["ray/tune/evaluation/episode_len_mean"],
+                        "Success Rate": ["ray/tune/evaluation/custom_metrics/success_mean"],
+                        "Slow Rate": ["ray/tune/evaluation/custom_metrics/slow_mean"],
+                        "Collision Rate": ["ray/tune/evaluation/custom_metrics/collision_mean",
+                                           "evaluation/collision_0"],
                         }
     data_from_subdirectories = {}
+
+    mean_curves = {}
+    # Load tensorboard data for each model and store in a list
+    for subdirectory in subdirectories:
+        if subdirectory not in data_from_subdirectories.keys():
+            # if "grid" in subdirectory:
+            data_from_subdirectories[subdirectory] = load_tensorboard_data_from_pkl(subdirectory,
+                                                                                    file_name=saved_file_name)
     # Initialize lists to store mean curves for each model
-    for key in names_for_y_axes.keys():
-        mean_curves = {}
-        # Load tensorboard data for each model and store in a list
-        for subdirectory in subdirectories:
-            if subdirectory not in data_from_subdirectories.keys():
-                data_from_subdirectories[subdirectory] = load_tensorboard_data_from_pkl(subdirectory, file_name=saved_file_name)
-            if "grid" in subdirectory:
-                mean_curves[subdirectory] = data_from_subdirectories[subdirectory]
-        #
+    for plot_key, possible_keys in names_for_y_axes.items():
         # Plotting
         plt.figure(figsize=(10, 6))
         # Plot the mean curve for each model
-        for model, dict_x in mean_curves.items():
-            if dict_x[timestep_key].ndim < 2:
-                dict_x = pd.concat([dict_x, dict_x], axis=1)
-            mask = (dict_x[timestep_key] < 0.2e5).mean(axis=1) > 0
-            dict_values = dict_x[key][mask]
-            # Calculate mean curve
-            mean_steps = dict_x[timestep_key][mask].mean(axis=1)
-            mean_values = dict_values.mean(axis=1)
-            std_values = dict_values.std(axis=1)
-            min_values = dict_values.min(axis=1)
-            max_values = dict_values.max(axis=1)
-            # if std_values.sum() == 0:
-            #     # add random noise to std_values to avoid error between 0 and 0.1
-            #     noise = np.random.rand(std_values.shape[0]) * 0.1
-            #     std_values += noise
-            #     min_values -= noise
-            #     max_values += noise
+        for model, dict_x in data_from_subdirectories.items():
+            for key in possible_keys:
+                if key in dict_x:
+                    # Find the first matching timestep key or use default
+                    time_steps = next(
+                        (dict_x[timestep_key] for timestep_key in timestep_keys if timestep_key in dict_x),
+                        pd.DataFrame(dict_x.index, columns=["step"], index=dict_x.index))
 
-            # Plot shaded area for mean curve
-            plt.fill_between(mean_steps, np.clip(mean_values - std_values, min_values, max_values),
-                             np.clip(mean_values + std_values, min_values, max_values), alpha=0.3,
-                             label=f'{os.path.split(model)[-1]}')
+                    # Ensure time_steps and dict_x have 2 dimensions
+                    if dict_x[key].ndim < 2:
+                        time_steps = pd.concat([time_steps, time_steps], axis=1)
+                        dict_x = pd.concat([dict_x, dict_x], axis=1)
 
-            # Plot mean curve
-            plt.plot(mean_steps, mean_values)
+                    # Apply mask for time_steps less than 0.2e5
+                    mask = (time_steps < 0.2e5).mean(axis=1) > 0
+                    dict_values = dict_x[key][mask]
+
+                    # Calculate statistics
+                    mean_steps = time_steps[mask].mean(axis=1)
+                    mean_values = dict_values.mean(axis=1)
+                    std_values = dict_values.std(axis=1)
+                    min_values = dict_values.min(axis=1)
+                    max_values = dict_values.max(axis=1)
+                    # if std_values.sum() == 0:
+                    #     # add random noise to std_values to avoid error between 0 and 0.1
+                    #     noise = np.random.rand(std_values.shape[0]) * 0.1
+                    #     std_values += noise
+                    #     min_values -= noise
+                    #     max_values += noise
+
+                    # Plot shaded area for mean curve
+                    plt.fill_between(mean_steps, np.clip(mean_values - std_values, min_values, max_values),
+                                     np.clip(mean_values + std_values, min_values, max_values), alpha=0.3,
+                                     label=f'{os.path.split(model)[-1]}')
+
+                    # Plot mean curve
+                    plt.plot(mean_steps, mean_values)
 
         # Set plot title and labels
         plt.title('Averaged Training Curves with 10 different seeds')
-        plt.xlabel(f'{timestep_key.split("/")[-1].replace("_", " ")} [-]')
-        plt.ylabel(f'{names_for_y_axes[key]}')
+        plt.xlabel(f'{timestep_keys[0].split("/")[-1].replace("_", " ")} [-]')
+        plt.ylabel(f'{plot_key}')
 
         # Add legend
         plt.legend()
